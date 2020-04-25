b710768e863809ce4818e1b86e2cef0998f483ac
Drum Ogilvie <me@daogilvie.com>
2020-04-25 16:33:05 +0100

    Chapter 1.3 completed!
    We now have barbaric and ugly lexing of the following monkey source:
    ```
    let five = 5;
    
    let ten = 10;
    
    let add = fn(x, y) {
      x + y;
    };
    
    let result = add(five, ten);
    
    ";
    ```
    An additional test is added to verify this!
    This code is doubtless not rust-esque, but I'm making progress through
    the book and will worry about that later — this puts me at the start of
    chapter 1.4.

--- src/lex.rs
+++ src/lex.rs
@@ -1,6 +1,7 @@
+use std::iter::Peekable;
 use std::str::Chars;
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone)]
 enum TokenType {
     ILLEGAL,
     EOF,
@@ -38,72 +39,138 @@ impl Token {
 }
 
 struct Lexer<'a> {
-    input_iter: Chars<'a>,
+    input_iter: Peekable<Chars<'a>>,
 }
 
 impl<'a> Lexer<'a> {
     fn for_str(input: &str) -> Lexer {
         Lexer {
-            input_iter: input.chars(),
+            input_iter: input.chars().peekable(),
         }
     }
 
     fn for_string(input: &'a String) -> Lexer<'a> {
         Lexer {
-            input_iter: input.chars(),
+            input_iter: input.chars().peekable(),
         }
     }
 
-    fn next_token(&mut self) -> Token {
-        match (self.read_char()) {
-            None => Token {
+    fn next_token(&mut self) -> Result<Token, &'static str> {
+        self.skip_whitespace();
+
+        match self.read_char() {
+            None => Ok(Token {
                 token_type: TokenType::EOF,
                 literal: None,
-            },
+            }),
             Some(ch) => {
                 let mut lit = String::with_capacity(1);
                 lit.push(ch);
                 let literal = Some(lit);
-                match (ch) {
-                    '=' => Token {
+                match ch {
+                    '=' => Ok(Token {
                         token_type: TokenType::ASSIGN,
                         literal,
-                    },
-                    '+' => Token {
+                    }),
+                    '+' => Ok(Token {
                         token_type: TokenType::PLUS,
                         literal,
-                    },
-                    '(' => Token {
+                    }),
+                    '(' => Ok(Token {
                         token_type: TokenType::LPAREN,
                         literal,
-                    },
-                    ')' => Token {
+                    }),
+                    ')' => Ok(Token {
                         token_type: TokenType::RPAREN,
                         literal,
-                    },
-                    '{' => Token {
+                    }),
+                    '{' => Ok(Token {
                         token_type: TokenType::LBRACE,
                         literal,
-                    },
-                    '}' => Token {
+                    }),
+                    '}' => Ok(Token {
                         token_type: TokenType::RBRACE,
                         literal,
-                    },
-                    ',' => Token {
+                    }),
+                    ',' => Ok(Token {
                         token_type: TokenType::COMMA,
                         literal,
-                    },
-                    ';' => Token {
+                    }),
+                    ';' => Ok(Token {
                         token_type: TokenType::SEMICOLON,
                         literal,
+                    }),
+                    l if l.is_alphabetic() => match self.read_ident(ch) {
+                        Some(ident) => Ok(Token {
+                            token_type: Lexer::get_ident_tokentype(&ident),
+                            literal: Some(ident),
+                        }),
+                        None => Err("cannot read ident"),
                     },
-                    _ => Token {
+                    d if d.is_digit(10) => Ok(Token {
+                        token_type: TokenType::INT,
+                        literal: self.read_int(ch, 10),
+                    }),
+                    _ => Ok(Token {
                         token_type: TokenType::ILLEGAL,
                         literal,
-                    },
+                    }),
                 }
             }
         }
+    }
+
+    fn skip_whitespace(&mut self) {
+        loop {
+            let next_char = self.peek_char();
+            match next_char {
+                None => break,
+                Some(c) => match c {
+                    w if w.is_whitespace() => {
+                        self.read_char();
+                    }
+                    _ => break,
+                },
+            }
+        }
+    }
+
+    fn read_ident(&mut self, prefix: char) -> Option<String> {
+        let mut ident = String::from("");
+        ident.push(prefix);
+        while let Some(&chr) = self.peek_char() {
+            if chr.is_alphabetic() {
+                ident.push(self.read_char()?);
+            } else {
+                break;
+            }
+        }
+        Some(ident)
+    }
+
+    fn get_ident_tokentype(ident: &String) -> TokenType {
+        match ident.as_str() {
+            "let" => TokenType::LET,
+            "fn" => TokenType::FUNCTION,
+            _ => TokenType::IDENT,
+        }
+    }
+
+    fn read_int(&mut self, prefix: char, radix: u32) -> Option<String> {
+        let mut ident = String::from("");
+        ident.push(prefix);
+        while let Some(&chr) = self.peek_char() {
+            if chr.is_digit(radix) {
+                ident.push(self.read_char()?);
+            } else {
+                break;
+            }
+        }
+        Some(ident)
+    }
+
+    fn peek_char(&mut self) -> Option<&char> {
+        self.input_iter.peek()
     }
 
     fn read_char(&mut self) -> Option<char> {
@@ -131,7 +198,71 @@ mod tests {
         ];
 
         for token in expected_tokens.iter() {
-            let parsed = lexer.next_token();
+            let parsed = lexer.next_token().unwrap();
+            assert_eq!(&parsed, token);
+        }
+    }
+
+    #[test]
+    fn test_lex_simple_script() {
+        let input = "let five = 5;
+        
+        let ten = 10;
+
+        
+        let add = fn(x, y) {
+          x + y;
+        };
+
+        let result = add(five, ten);
+
+        ";
+        let mut lexer = Lexer::for_str(input);
+        let expected_tokens = [
+            Token::from_pair(TokenType::LET, String::from("let")),
+            Token::from_pair(TokenType::IDENT, String::from("five")),
+            Token::from_pair(TokenType::ASSIGN, String::from("=")),
+            Token::from_pair(TokenType::INT, String::from("5")),
+            Token::from_pair(TokenType::SEMICOLON, String::from(";")),
+            Token::from_pair(TokenType::LET, String::from("let")),
+            Token::from_pair(TokenType::IDENT, String::from("ten")),
+            Token::from_pair(TokenType::ASSIGN, String::from("=")),
+            Token::from_pair(TokenType::INT, String::from("10")),
+            Token::from_pair(TokenType::SEMICOLON, String::from(";")),
+            Token::from_pair(TokenType::LET, String::from("let")),
+            Token::from_pair(TokenType::IDENT, String::from("add")),
+            Token::from_pair(TokenType::ASSIGN, String::from("=")),
+            Token::from_pair(TokenType::FUNCTION, String::from("fn")),
+            Token::from_pair(TokenType::LPAREN, String::from("(")),
+            Token::from_pair(TokenType::IDENT, String::from("x")),
+            Token::from_pair(TokenType::COMMA, String::from(",")),
+            Token::from_pair(TokenType::IDENT, String::from("y")),
+            Token::from_pair(TokenType::RPAREN, String::from(")")),
+            Token::from_pair(TokenType::LBRACE, String::from("{")),
+            Token::from_pair(TokenType::IDENT, String::from("x")),
+            Token::from_pair(TokenType::PLUS, String::from("+")),
+            Token::from_pair(TokenType::IDENT, String::from("y")),
+            Token::from_pair(TokenType::SEMICOLON, String::from(";")),
+            Token::from_pair(TokenType::RBRACE, String::from("}")),
+            Token::from_pair(TokenType::SEMICOLON, String::from(";")),
+            Token::from_pair(TokenType::LET, String::from("let")),
+            Token::from_pair(TokenType::IDENT, String::from("result")),
+            Token::from_pair(TokenType::ASSIGN, String::from("=")),
+            Token::from_pair(TokenType::IDENT, String::from("add")),
+            Token::from_pair(TokenType::LPAREN, String::from("(")),
+            Token::from_pair(TokenType::IDENT, String::from("five")),
+            Token::from_pair(TokenType::COMMA, String::from(",")),
+            Token::from_pair(TokenType::IDENT, String::from("ten")),
+            Token::from_pair(TokenType::RPAREN, String::from(")")),
+            Token::from_pair(TokenType::SEMICOLON, String::from(";")),
+            Token {
+                token_type: TokenType::EOF,
+                literal: None,
+            },
+        ];
+
+        for token in expected_tokens.iter() {
+            let parsed = lexer.next_token().unwrap();
             assert_eq!(&parsed, token);
         }
     }
